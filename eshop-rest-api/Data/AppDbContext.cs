264af0377f7c9b8117b7388dc8681669542bf2da
djansaa <davidjansaa@gmail.com>
2025-10-11 01:57:16 +0200

    Add db context

---
+++ eshop-rest-api/Data/AppDbContext.cs
@@ -0,0 +1,27 @@
+﻿using eshop_rest_api.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eshop_rest_api.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Product> Products => Set<Product>();
+
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder mb)
+        {
+            mb.Entity<Product>(e =>
+            {
+                e.Property(p => p.Name).IsRequired();
+                e.Property(p => p.ImgUri).IsRequired();
+                e.Property(p => p.Price).IsRequired();
+            });
+        }
+    }
+}
