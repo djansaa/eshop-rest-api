aa55db92fb13d5619ca1907b6b76c21e302a02b8
djansaa <davidjansaa@gmail.com>
2025-10-11 02:39:49 +0200

    Add services

---
+++ eshop-rest-api/Services/IProductService.cs
@@ -0,0 +1,16 @@
+﻿using eshop_rest_api.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eshop_rest_api.Services
+{
+    public interface IProductService
+    {
+        Task<List<Product>> GetAllAsync();
+        Task<Product?> GetByIdAsync(int id);
+        Task<Product?> UpdateDescriptionAsync(int id, string description);
+    }
+}
---
+++ eshop-rest-api/Services/ProductService.cs
@@ -0,0 +1,42 @@
+﻿using eshop_rest_api.Data;
+using eshop_rest_api.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eshop_rest_api.Services
+{
+    public class ProductService : IProductService
+    {
+        private readonly AppDbContext _db;
+        public ProductService(AppDbContext db)
+        {
+            _db = db;
+        }
+
+        public Task<List<Product>> GetAllAsync()
+        {
+            return _db.Products.AsNoTracking().ToListAsync();
+        }
+
+        public Task<Product?> GetByIdAsync(int id)
+        {
+            return _db.Products.AsNoTracking().FirstOrDefaultAsync(p => p.Id == id);
+        }
+
+        public async Task<Product?> UpdateDescriptionAsync(int id, string description)
+        {
+            var p = await _db.Products.FirstOrDefaultAsync(p => p.Id == id);
+            if (p is null) return null;
+
+            p.Description = description;
+            await _db.SaveChangesAsync();
+
+            return p;
+
+        }
+    }
+}
