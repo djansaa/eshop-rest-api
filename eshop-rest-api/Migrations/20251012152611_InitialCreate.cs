fc7c05618a9ef3346d9bd8b6163afc4cb9ab99d4
djansaa <davidjansaa@gmail.com>
2025-10-12 17:57:43 +0200

    Add migrations

---
+++ eshop-rest-api/Migrations/20251012152611_InitialCreate.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace eshop_rest_api.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Products",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false),
+                    Name = table.Column<string>(type: "TEXT", nullable: false),
+                    ImgUri = table.Column<string>(type: "TEXT", nullable: false),
+                    Price = table.Column<decimal>(type: "TEXT", nullable: false),
+                    Description = table.Column<string>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Products", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Products",
+                columns: new[] { "Id", "Description", "ImgUri", "Name", "Price" },
+                values: new object[,]
+                {
+                    { 101, "D0", "https://placeholder.com/0", "P0", 58.15m },
+                    { 102, "D1", "https://placeholder.com/1", "P1", 82.37m },
+                    { 103, "D2", "https://placeholder.com/2", "P2", 95.81m },
+                    { 104, "D3", "https://placeholder.com/3", "P3", 82.40m },
+                    { 105, "D4", "https://placeholder.com/4", "P4", 87.63m },
+                    { 106, "D5", "https://placeholder.com/5", "P5", 23.55m },
+                    { 107, "D6", "https://placeholder.com/6", "P6", 6.42m },
+                    { 108, "D7", "https://placeholder.com/7", "P7", 6.92m },
+                    { 109, "D8", "https://placeholder.com/8", "P8", 79.28m },
+                    { 110, "D9", "https://placeholder.com/9", "P9", 91.53m },
+                    { 111, "D10", "https://placeholder.com/10", "P10", 87.65m },
+                    { 112, "D11", "https://placeholder.com/11", "P11", 27.14m },
+                    { 113, "D12", "https://placeholder.com/12", "P12", 45.33m },
+                    { 114, "D13", "https://placeholder.com/13", "P13", 38.63m },
+                    { 115, "D14", "https://placeholder.com/14", "P14", 96.95m },
+                    { 116, "D15", "https://placeholder.com/15", "P15", 92.34m },
+                    { 117, "D16", "https://placeholder.com/16", "P16", 13.02m },
+                    { 118, "D17", "https://placeholder.com/17", "P17", 92.24m },
+                    { 119, "D18", "https://placeholder.com/18", "P18", 11.17m },
+                    { 120, "D19", "https://placeholder.com/19", "P19", 64.34m },
+                    { 121, "D20", "https://placeholder.com/20", "P20", 28.44m },
+                    { 122, "D21", "https://placeholder.com/21", "P21", 76.32m },
+                    { 123, "D22", "https://placeholder.com/22", "P22", 86.46m },
+                    { 124, "D23", "https://placeholder.com/23", "P23", 35.84m },
+                    { 125, "D24", "https://placeholder.com/24", "P24", 50.07m }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Products");
+        }
+    }
+}
---
+++ eshop-rest-api/Migrations/20251012153437_SyncModel_Seed25.cs
@@ -0,0 +1,368 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace eshop_rest_api.Migrations
+{
+    /// <inheritdoc />
+    public partial class SyncModel_Seed25 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 101,
+                columns: new[] { "Description", "ImgUri", "Name", "Price" },
+                values: new object[] { "Sample 101", "https://placeholder.com/101", "Product 101", 49.90m });
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 102,
+                columns: new[] { "Description", "ImgUri", "Name", "Price" },
+                values: new object[] { "Sample 102", "https://placeholder.com/102", "Product 102", 54.90m });
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 103,
+                columns: new[] { "Description", "ImgUri", "Name", "Price" },
+                values: new object[] { "Sample 103", "https://placeholder.com/103", "Product 103", 59.90m });
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 104,
+                columns: new[] { "Description", "ImgUri", "Name", "Price" },
+                values: new object[] { "Sample 104", "https://placeholder.com/104", "Product 104", 64.90m });
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 105,
+                columns: new[] { "Description", "ImgUri", "Name", "Price" },
+                values: new object[] { "Sample 105", "https://placeholder.com/105", "Product 105", 69.90m });
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 106,
+                columns: new[] { "Description", "ImgUri", "Name", "Price" },
+                values: new object[] { "Sample 106", "https://placeholder.com/106", "Product 106", 74.90m });
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 107,
+                columns: new[] { "Description", "ImgUri", "Name", "Price" },
+                values: new object[] { "Sample 107", "https://placeholder.com/107", "Product 107", 79.90m });
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 108,
+                columns: new[] { "Description", "ImgUri", "Name", "Price" },
+                values: new object[] { "Sample 108", "https://placeholder.com/108", "Product 108", 84.90m });
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 109,
+                columns: new[] { "Description", "ImgUri", "Name", "Price" },
+                values: new object[] { "Sample 109", "https://placeholder.com/109", "Product 109", 89.90m });
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 110,
+                columns: new[] { "Description", "ImgUri", "Name", "Price" },
+                values: new object[] { "Sample 110", "https://placeholder.com/110", "Product 110", 94.90m });
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 111,
+                columns: new[] { "Description", "ImgUri", "Name", "Price" },
+                values: new object[] { "Sample 111", "https://placeholder.com/111", "Product 111", 99.90m });
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 112,
+                columns: new[] { "Description", "ImgUri", "Name", "Price" },
+                values: new object[] { "Sample 112", "https://placeholder.com/112", "Product 112", 104.90m });
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 113,
+                columns: new[] { "Description", "ImgUri", "Name", "Price" },
+                values: new object[] { "Sample 113", "https://placeholder.com/113", "Product 113", 109.90m });
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 114,
+                columns: new[] { "Description", "ImgUri", "Name", "Price" },
+                values: new object[] { "Sample 114", "https://placeholder.com/114", "Product 114", 114.90m });
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 115,
+                columns: new[] { "Description", "ImgUri", "Name", "Price" },
+                values: new object[] { "Sample 115", "https://placeholder.com/115", "Product 115", 119.90m });
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 116,
+                columns: new[] { "Description", "ImgUri", "Name", "Price" },
+                values: new object[] { "Sample 116", "https://placeholder.com/116", "Product 116", 124.90m });
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 117,
+                columns: new[] { "Description", "ImgUri", "Name", "Price" },
+                values: new object[] { "Sample 117", "https://placeholder.com/117", "Product 117", 129.90m });
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 118,
+                columns: new[] { "Description", "ImgUri", "Name", "Price" },
+                values: new object[] { "Sample 118", "https://placeholder.com/118", "Product 118", 134.90m });
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 119,
+                columns: new[] { "Description", "ImgUri", "Name", "Price" },
+                values: new object[] { "Sample 119", "https://placeholder.com/119", "Product 119", 139.90m });
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 120,
+                columns: new[] { "Description", "ImgUri", "Name", "Price" },
+                values: new object[] { "Sample 120", "https://placeholder.com/120", "Product 120", 144.90m });
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 121,
+                columns: new[] { "Description", "ImgUri", "Name", "Price" },
+                values: new object[] { "Sample 121", "https://placeholder.com/121", "Product 121", 149.90m });
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 122,
+                columns: new[] { "Description", "ImgUri", "Name", "Price" },
+                values: new object[] { "Sample 122", "https://placeholder.com/122", "Product 122", 154.90m });
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 123,
+                columns: new[] { "Description", "ImgUri", "Name", "Price" },
+                values: new object[] { "Sample 123", "https://placeholder.com/123", "Product 123", 159.90m });
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 124,
+                columns: new[] { "Description", "ImgUri", "Name", "Price" },
+                values: new object[] { "Sample 124", "https://placeholder.com/124", "Product 124", 164.90m });
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 125,
+                columns: new[] { "Description", "ImgUri", "Name", "Price" },
+                values: new object[] { "Sample 125", "https://placeholder.com/125", "Product 125", 169.90m });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 101,
+                columns: new[] { "Description", "ImgUri", "Name", "Price" },
+                values: new object[] { "D0", "https://placeholder.com/0", "P0", 58.15m });
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 102,
+                columns: new[] { "Description", "ImgUri", "Name", "Price" },
+                values: new object[] { "D1", "https://placeholder.com/1", "P1", 82.37m });
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 103,
+                columns: new[] { "Description", "ImgUri", "Name", "Price" },
+                values: new object[] { "D2", "https://placeholder.com/2", "P2", 95.81m });
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 104,
+                columns: new[] { "Description", "ImgUri", "Name", "Price" },
+                values: new object[] { "D3", "https://placeholder.com/3", "P3", 82.40m });
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 105,
+                columns: new[] { "Description", "ImgUri", "Name", "Price" },
+                values: new object[] { "D4", "https://placeholder.com/4", "P4", 87.63m });
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 106,
+                columns: new[] { "Description", "ImgUri", "Name", "Price" },
+                values: new object[] { "D5", "https://placeholder.com/5", "P5", 23.55m });
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 107,
+                columns: new[] { "Description", "ImgUri", "Name", "Price" },
+                values: new object[] { "D6", "https://placeholder.com/6", "P6", 6.42m });
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 108,
+                columns: new[] { "Description", "ImgUri", "Name", "Price" },
+                values: new object[] { "D7", "https://placeholder.com/7", "P7", 6.92m });
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 109,
+                columns: new[] { "Description", "ImgUri", "Name", "Price" },
+                values: new object[] { "D8", "https://placeholder.com/8", "P8", 79.28m });
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 110,
+                columns: new[] { "Description", "ImgUri", "Name", "Price" },
+                values: new object[] { "D9", "https://placeholder.com/9", "P9", 91.53m });
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 111,
+                columns: new[] { "Description", "ImgUri", "Name", "Price" },
+                values: new object[] { "D10", "https://placeholder.com/10", "P10", 87.65m });
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 112,
+                columns: new[] { "Description", "ImgUri", "Name", "Price" },
+                values: new object[] { "D11", "https://placeholder.com/11", "P11", 27.14m });
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 113,
+                columns: new[] { "Description", "ImgUri", "Name", "Price" },
+                values: new object[] { "D12", "https://placeholder.com/12", "P12", 45.33m });
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 114,
+                columns: new[] { "Description", "ImgUri", "Name", "Price" },
+                values: new object[] { "D13", "https://placeholder.com/13", "P13", 38.63m });
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 115,
+                columns: new[] { "Description", "ImgUri", "Name", "Price" },
+                values: new object[] { "D14", "https://placeholder.com/14", "P14", 96.95m });
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 116,
+                columns: new[] { "Description", "ImgUri", "Name", "Price" },
+                values: new object[] { "D15", "https://placeholder.com/15", "P15", 92.34m });
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 117,
+                columns: new[] { "Description", "ImgUri", "Name", "Price" },
+                values: new object[] { "D16", "https://placeholder.com/16", "P16", 13.02m });
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 118,
+                columns: new[] { "Description", "ImgUri", "Name", "Price" },
+                values: new object[] { "D17", "https://placeholder.com/17", "P17", 92.24m });
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 119,
+                columns: new[] { "Description", "ImgUri", "Name", "Price" },
+                values: new object[] { "D18", "https://placeholder.com/18", "P18", 11.17m });
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 120,
+                columns: new[] { "Description", "ImgUri", "Name", "Price" },
+                values: new object[] { "D19", "https://placeholder.com/19", "P19", 64.34m });
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 121,
+                columns: new[] { "Description", "ImgUri", "Name", "Price" },
+                values: new object[] { "D20", "https://placeholder.com/20", "P20", 28.44m });
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 122,
+                columns: new[] { "Description", "ImgUri", "Name", "Price" },
+                values: new object[] { "D21", "https://placeholder.com/21", "P21", 76.32m });
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 123,
+                columns: new[] { "Description", "ImgUri", "Name", "Price" },
+                values: new object[] { "D22", "https://placeholder.com/22", "P22", 86.46m });
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 124,
+                columns: new[] { "Description", "ImgUri", "Name", "Price" },
+                values: new object[] { "D23", "https://placeholder.com/23", "P23", 35.84m });
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 125,
+                columns: new[] { "Description", "ImgUri", "Name", "Price" },
+                values: new object[] { "D24", "https://placeholder.com/24", "P24", 50.07m });
+        }
+    }
+}
