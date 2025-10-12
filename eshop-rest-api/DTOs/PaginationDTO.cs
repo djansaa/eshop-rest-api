7a32d1ce87fb7226855004515ba2db0fb242735a
djansaa <davidjansaa@gmail.com>
2025-10-12 11:42:30 +0200

    Update dtos - required fields and comments

--- eshop-rest-api/DTOs/PaginationDTO.cs
+++ eshop-rest-api/DTOs/PaginationDTO.cs
@@ -1,13 +1,47 @@
 ﻿using Microsoft.AspNetCore.Mvc.ApplicationModels;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace eshop_rest_api.DTOs
 {
-    public record PaginationLinksDTO(string? Self, string? First, string? Previous, string? Next, string? Last);
-    public record PaginationDTO(int Page, int PageSize, int TotalPages, PaginationLinksDTO Links);
-    public record PagedDTO<T>(IEnumerable<T> Data, PaginationDTO Meta);
+    /// <summary>
+    /// Represents a set of pagination links for navigating through a paginated resource.
+    /// </summary>
+    /// <remarks>This DTO provides links to the current page, the first page, the last page, and optionally
+    /// the previous and next pages. It is typically used in APIs to facilitate client-side navigation of paginated
+    /// data.</remarks>
+    /// <param name="Self"></param>
+    /// <param name="First"></param>
+    /// <param name="Previous"></param>
+    /// <param name="Next"></param>
+    /// <param name="Last"></param>
+    public record PaginationLinksDTO([property: Required] string Self, [property: Required] string First, string? Previous, string? Next, [property: Required] string Last);
+
+    /// <summary>
+    /// Represents pagination details for a paginated response, including the current page, page size, total pages, and
+    /// navigation links.
+    /// </summary>
+    /// <remarks>This record is typically used to encapsulate metadata about paginated data, such as the
+    /// current page number, the number of items per page, the total number of pages available, and links for navigating
+    /// between pages.</remarks>
+    /// <param name="Page"></param>
+    /// <param name="PageSize"></param>
+    /// <param name="TotalPages"></param>
+    /// <param name="Links"></param>
+    public record PaginationDTO([property: Required] int Page, [property: Required] int PageSize, [property: Required] int TotalPages, [property: Required] PaginationLinksDTO Links);
+
+    /// <summary>
+    /// Represents a paginated response containing a collection of data items and associated pagination metadata.
+    /// </summary>
+    /// <remarks>This record is typically used to encapsulate a paginated result set, where the <see
+    /// cref="Data"/> property contains the items for the current page, and the <see cref="Meta"/> property provides
+    /// details about the pagination state, such as the current page number, total pages, and total items.</remarks>
+    /// <typeparam name="T">The type of the data items contained in the paginated response.</typeparam>
+    /// <param name="Data"></param>
+    /// <param name="Meta"></param>
+    public record PagedDTO<T>([property: Required] IEnumerable<T> Data, [property: Required] PaginationDTO Meta);
 }
--- eshop-rest-api/DTOs/ProductDTO.cs
+++ eshop-rest-api/DTOs/ProductDTO.cs
@@ -1,6 +1,25 @@
 ﻿
+using System.ComponentModel.DataAnnotations;
+
 namespace eshop_rest_api.DTOs
 {
-    public record ProductDTO(int Id, string Name, string ImgUri, decimal Price, string? Description);
-    public record UpdateProductDescriptionDTO(string Description);
+    /// <summary>
+    /// Represents a data transfer object for a product, containing essential product details.
+    /// </summary>
+    /// <remarks>This record is typically used to transfer product information between application layers. All
+    /// required properties must be provided when creating an instance of this record.</remarks>
+    /// <param name="Id"></param>
+    /// <param name="Name"></param>
+    /// <param name="ImgUri"></param>
+    /// <param name="Price"></param>
+    /// <param name="Description"></param>
+    public record ProductDTO([property: Required] int Id, [property: Required] string Name, [property: Required] string ImgUri, [property: Required] decimal Price, string? Description);
+
+    /// <summary>
+    /// Represents the data transfer object for updating a product's description.
+    /// </summary>
+    /// <remarks>This DTO is used to encapsulate the new description of a product when performing an update
+    /// operation. The description can be null to indicate that the product's description should be cleared.</remarks>
+    /// <param name="Description">The new description for the product. Can be null to remove the existing description.</param>
+    public record UpdateProductDescriptionDTO(string? Description);
 }
