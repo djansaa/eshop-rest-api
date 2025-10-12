cdb5c8067b38223d9f9e0d18a9ea5ec0fcee3c65
djansaa <davidjansaa@gmail.com>
2025-10-12 17:56:51 +0200

    Add product model required attributes

--- eshop-rest-api/Models/Product.cs
+++ eshop-rest-api/Models/Product.cs
@@ -1,11 +1,21 @@
-﻿namespace eshop_rest_api.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace eshop_rest_api.Models
 {
     public class Product
     {
+        [Required]
         public int Id { get; set; }
+
+        [Required]
         public string Name { get; set; }
+
+        [Required]
         public string ImgUri { get; set; }
+
+        [Required]
         public decimal Price { get; set; }
+
         public string? Description { get; set; }
     }
 }
